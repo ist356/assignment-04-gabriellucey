22e2e8b039272e1dc5dc0e656b60b685cc54fcfc
gabriellucey <119355862+gabriellucey@users.noreply.github.com>
2024-09-29 20:12:17 -0400

    completed unibrow application.py with 3 inputs and two outputs.

--- code/unibrow.py
+++ code/unibrow.py
@@ -9,17 +9,6 @@ st.title("UniBrow")
 st.caption("The Universal data browser")
 
 file_upload = st.file_uploader("Upload a file", type=['csv', 'xlsx', 'json'])
-#The UniBrow application is a streamlit application which displays a data from a file.
-
-#It consists of 3 inputs:
-
-#upload a file in Excel (XLSX), Comma-Separared, with Header (CSV), or Row-Oriented Json (JSON) into a dataframe
-#select which columns to display from the dataframe
-#build a fiter on the dataframe based on a text column and one of the values in the column.
-#And 2 outputs:
-
-#the dataframe with column / row filters applied.
-#the describe of the dataframe (to see statistics for the numerical columns)
 
 if file_upload:
     df = pl.load_file(file_upload, pl.get_file_extension(file_upload.name))
@@ -28,15 +17,15 @@ if file_upload:
     selected_cols = st.multiselect("Select columns to display", cols)
     
     if selected_cols:
-        df_filtered = df[selected_cols]
-        st.write(df_filtered)
-        st.write("DataFrame Description:")
-        st.write(df_filtered.describe())
-        
         filter_col = st.selectbox("Select a column to filter", selected_cols)
         filter_val = st.text_input("Enter a value to filter")
         
-        if st.button("Filter"):
-            st.write(df_filtered[df_filtered[filter_col] == filter_val])
-
+        if filter_val:
+            df_filtered = df[selected_cols][df[selected_cols][filter_col] == filter_val]
+        else:
+            df_filtered = df[selected_cols]
+        
+        st.write(df_filtered)
+        st.write("DataFrame Description:")
+        st.write(df_filtered.describe())
 
